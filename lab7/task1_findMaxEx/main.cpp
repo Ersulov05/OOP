35ec658aab422d4d35dfe669555fee0562cae501
Ersulov05 <ersulov05@mail.ru>
2025-05-22 08:48:04 +0300

    Lab7-task1-findMaxEx Перенес реализацию шаблонной функции в заголовочный файл

--- lab7/task1_findMaxEx/main.cpp
+++ lab7/task1_findMaxEx/main.cpp
@@ -39,8 +39,8 @@ int main()
 {
 	std::vector<Sportsmen> sportsmens;
 	sportsmens.push_back(Sportsmen("Maryshev Alexey", 175, 80));
-	sportsmens.push_back(Sportsmen("Ersulov Andrey", 160, 55));
-	sportsmens.push_back(Sportsmen("Dudinov Nikita", 175, 70));
+	sportsmens.push_back(Sportsmen("Ersulov Andrey", 160, 95));
+	sportsmens.push_back(Sportsmen("Dudinov Nikita", 180, 70));
 	sportsmens.push_back(Sportsmen("Belyaev Dmitriy", 165, 65));
 
 	Sportsmen maxSportsmen;
@@ -52,7 +52,7 @@ int main()
 		std::cout << std::endl;
 	}
 
-	if (FindMaxEx(sportsmens, maxSportsmen, CompareByHeight))
+	if (FindMaxEx(sportsmens, maxSportsmen, CompareByWeight))
 	{
 		std::cout << "Sportsmen with max weight ";
 		PrintSportsmen(std::cout, maxSportsmen);
--- lab7/task1_findMaxEx/src/FindMaxEx.h
+++ lab7/task1_findMaxEx/src/FindMaxEx.h
@@ -2,4 +2,22 @@
 #include <vector>
 
 template <typename T, typename Less>
-inline bool FindMaxEx(const std::vector<T>& arr, T& maxValue, const Less& less);
+inline bool FindMaxEx(const std::vector<T>& arr, T& maxValue, const Less& less)
+{
+	if (arr.empty())
+	{
+		return false;
+	}
+
+	auto max = arr[0];
+	for (const auto& elem : arr)
+	{
+		if (less(max, elem))
+		{
+			max = elem;
+		}
+	}
+	maxValue = max;
+
+	return true;
+}
